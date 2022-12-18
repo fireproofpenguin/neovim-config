6e894a0ccd1a446883c2031447bd142b2b8ba83a
Sam Gerrard <sam.gerrard@trustedhousesitters.com>
2022-12-18 11:41:14 +0000

    Remapped harpoon and changed search case

--- lua/sam/remap.lua
+++ lua/sam/remap.lua
@@ -2,6 +2,7 @@ local nnoremap = require("sam.keymap").nnoremap
 local tnoremap = require("sam.keymap").tnoremap
 
 nnoremap("<leader>pv", "<cmd>NvimTreeToggle<CR>")
+nnoremap("<leader>pc", "<cmd>NvimTreeFindFile<CR>")
 tnoremap("<Esc>", "<C-\\><C-n>")
 
 -- Harpoon keybindings
@@ -9,15 +10,16 @@ nnoremap("<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>")
 nnoremap("<leader>m", "<cmd>lua require('harpoon.mark').add_file()<CR>")
 
 -- Harpoon bindings for navigating marks
-nnoremap("<C-1>", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
-nnoremap("<C-2>", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
-nnoremap("<C-3>", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
-nnoremap("<C-4>", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
-nnoremap("<C-5>", "<cmd>lua require('harpoon.ui').nav_file(5)<CR>")
-nnoremap("<C-6>", "<cmd>lua require('harpoon.ui').nav_file(6)<CR>")
-nnoremap("<C-7>", "<cmd>lua require('harpoon.ui').nav_file(7)<CR>")
-nnoremap("<C-8>", "<cmd>lua require('harpoon.ui').nav_file(8)<CR>")
-nnoremap("<C-9>", "<cmd>lua require('harpoon.ui').nav_file(9)<CR>")
+nnoremap("<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
+nnoremap("<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
+nnoremap("<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
+nnoremap("<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
+nnoremap("<leader>5", "<cmd>lua require('harpoon.ui').nav_file(5)<CR>")
+nnoremap("<leader>6", "<cmd>lua require('harpoon.ui').nav_file(6)<CR>")
+nnoremap("<leader>7", "<cmd>lua require('harpoon.ui').nav_file(7)<CR>")
+nnoremap("<leader>8", "<cmd>lua require('harpoon.ui').nav_file(8)<CR>")
+nnoremap("<leader>9", "<cmd>lua require('harpoon.ui').nav_file(9)<CR>")
+nnoremap("<leader>0", "<cmd>lua require('harpoon.term').gotoTerminal(2)<CR>")
 nnoremap("<C-0>", "<cmd>lua require('harpoon.term').gotoTerminal(1)<CR>")
 
 -- Harpoon next and previous buffer
